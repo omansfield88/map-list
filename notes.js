fb201e9caa4319000b8f2dee6d8baca30401bb6e
Oliver Mansfield <omansfield88@gmail.com>
2020-04-18 13:38:46 +0100

    Adding notes file for Zoopla api calls and stuff

---
+++ notes.js
@@ -0,0 +1,3 @@
+//53.369653, -2.907952   West alerton train station  min
+//53.388398, -2.950458   Princes park mansions	max
+//http://api.zoopla.co.uk/api/v1/property_listings.js?lat_min=53.369653&lat_max=53.388398&lon_min=-2.907952&lon_max=-2.950458&radius=1mile&api_key=APIKEY
